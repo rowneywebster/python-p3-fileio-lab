1459118ce520efe9744e41791afceada8474d579
Rowney Ouma <oumarowney@gmail.com>
2025-07-17 01:54:14 +0300

    All done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,20 @@
 def write_file(file_name, file_content):
-    pass
+    full_name = f"{file_name}.txt"
+    with open(full_name, mode="w", encoding="utf-8") as text_file:
+        text_file.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    full_name = f"{file_name}.txt"
+    with open(full_name, mode="a", encoding="utf-8") as text_file:
+        text_file.write(append_content)
+
+
 
 def read_file(file_name):
-    pass
+    full_name = f"{file_name}.txt"
+    content = ""
+    with open(full_name, mode="r", encoding="utf-8") as text_file:
+        for line in text_file:
+            content += line
+    return content
